7591dba47a53f0512fba49a28f38967b3a252fbf
aphrodite0720 <aleksandr.ivanovich19970720@gmail.com>
2022-01-04 02:58:52 -0500

    definite tree.js
    definite some of binary tree functions

---
+++ src/BinaryTree.js
@@ -0,0 +1,70 @@
+
+class BinaryTree {
+    constructor(x, y, backgroundColor) {
+        
+    }
+
+    // Sets this instance's reference to a Controls instance so that an
+    // animation interval can be set
+    bindControls(controls) {
+       
+    }
+
+    // Resets the root node to an empty node, effectively clearing all nodes
+    clear() {
+       
+    }
+
+    // Returns: a random number in the range [0, max) not yet in the tree
+    uniqueRandom(max) {
+       
+    }
+
+    // Quickly fills the tree with a certain number of nodes
+    fill(count) {
+       
+    }
+
+    // Wraps the Node class's addValue method, and sets the coordinate of the
+    // subset of the tree that needs to be adjusted after the value was added
+    addValue(value) {
+       
+    }
+
+
+    deleteValueVisual(value){
+       
+    }
+
+    searchPos(x,y){
+        
+    }
+
+    // Wraps the Node class's search method directly
+    search(value) {
+      
+    }
+
+    // their own position based off their parents (by passing no arguments)
+    setCoordinates(node) {
+        
+    }
+
+    // Draws the entire visulizatino, including the background, and each node
+    draw() {
+      
+    }
+
+    // Displays the tree without redrawing every node in the tree
+    // This function is used when the color of singular nodes are updated
+    updateDrawing() {
+       
+    }
+
+    // Wraps the Node class's resetVisuals method, and draws the result
+    resetVisuals() {
+       
+    }
+
+   
+}
