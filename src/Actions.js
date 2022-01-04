b965b7a6f65fe7a020035962abde2c2eecfb2d1a
aphrodite0720 <aleksandr.ivanovich19970720@gmail.com>
2022-01-04 04:49:24 -0500

    complete definition
    complete whole definition with three js files

---
+++ src/Actions.js
@@ -0,0 +1,55 @@
+
+class Actions {
+
+    constructor(tree) {
+        
+    }
+
+    // Completly resets the tree, removing all nodes, stopping all animations
+    clear() {
+       
+    }
+
+    // Called by event listeners to run a certain animation if one is not running
+    triggerAnimation(animation) {
+       
+    }
+
+       getNumber(text) {
+        
+    }
+
+    // Method for the Quick Fill animation
+    quickFill() {
+       
+    }
+
+    // Method for the Fill animation
+    slowFill() {
+      
+    }
+
+    // Method for the Add animation
+    add() {
+       
+    }
+
+    delete(val) {
+       
+    }
+
+
+    searchPos(x,y){
+       
+    }
+
+    // Method for the search animation
+    search() {
+       
+    }
+
+    // Inverts and exponentiates the linear output of the slider to set the interval
+    setAnimationSpeed() {
+       
+    }
+}
--- src/BinaryTree.js
+++ src/BinaryTree.js
@@ -1,70 +1,149 @@
 
 class BinaryTree {
     constructor(x, y, backgroundColor) {
-        
+       
     }
 
-    // Sets this instance's reference to a Controls instance so that an
-    // animation interval can be set
+  
     bindControls(controls) {
-       
+      
     }
 
     // Resets the root node to an empty node, effectively clearing all nodes
     clear() {
-       
+        
     }
 
     // Returns: a random number in the range [0, max) not yet in the tree
     uniqueRandom(max) {
-       
+        
     }
 
     // Quickly fills the tree with a certain number of nodes
     fill(count) {
-       
+        
     }
 
-    // Wraps the Node class's addValue method, and sets the coordinate of the
-    // subset of the tree that needs to be adjusted after the value was added
+    
     addValue(value) {
-       
+        
     }
 
 
     deleteValueVisual(value){
-       
+      
     }
 
     searchPos(x,y){
-        
+       
     }
 
     // Wraps the Node class's search method directly
     search(value) {
-      
+        
     }
 
-    // their own position based off their parents (by passing no arguments)
     setCoordinates(node) {
-        
+       
     }
 
     // Draws the entire visulizatino, including the background, and each node
     draw() {
-      
+     
     }
 
-    // Displays the tree without redrawing every node in the tree
-    // This function is used when the color of singular nodes are updated
+ 
     updateDrawing() {
        
     }
 
     // Wraps the Node class's resetVisuals method, and draws the result
     resetVisuals() {
+      
+    }
+
+  
+    startAnimation(frame, ...args) {
+       
+    }
+
+    
+    continueAnimation(frame, ...args) {
+        
+    }
+
+    
+    stopAnimation(complete = () => {}, ...callbackArgs) {
+        
+    }
+
+    addValueVisual(value, complete = () => {}, ...callbackArgs) {
+        
+    }
+
+    
+    addValueFrame(value, complete, ...callbackArgs) {
+        
+    }
+
+   
+    searchVisual(value, complete = () => {}, ...callbackArgs) {
+        
+    }
+
+   
+    searchFrame(value, complete, ...callbackArgs) {
+        
+    }
+
+    
+    fillVisual(count, complete = () => {}) {
+       
+    }
+
+    fillFrame(count, filled, complete) {
+       
+    }
+
+    startAnimation(frame, ...args) {
+       
+    }
+
+    // Schedules the next frame of the animation
+    continueAnimation(frame, ...args) {
        
     }
 
    
+    stopAnimation(complete = () => {}, ...callbackArgs) {
+        
+    }
+
+   
+    addValueVisual(value, complete = () => {}, ...callbackArgs) {
+        
+    }
+
+    addValueFrame(value, complete, ...callbackArgs) {
+        
+    }
+
+    searchVisual(value, complete = () => {}, ...callbackArgs) {
+       
+    }
+
+    searchFrame(value, complete, ...callbackArgs) {
+        
+    }
+
+    
+    fillVisual(count, complete = () => {}) {
+        
+    }
+
+    // complete is a callback called when the animation finishes
+    fillFrame(count, filled, complete) {
+        
+    }
+
 }
