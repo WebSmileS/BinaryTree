21b30a335e48eac2a93ba1ba4d60414401558265
aphrodite0720 <aleksandr.ivanovich19970720@gmail.com>
2022-01-04 01:49:40 -0500

    first_commit
    function definition of index

---
+++ src/Node.js
@@ -0,0 +1,88 @@
+
+
+class Node {
+    // Note: this section uses the static keyword to define static properties.
+  
+    constructor {
+
+        
+    }
+
+    // Definition of a "filled" node that should be processed recursively
+    isFilled() {
+        
+    }
+
+    // Checks if a node has a parent, or is the root of a tree
+    hasParent() {
+        
+    }
+
+   
+    addValue(value) {
+        
+         else if (value < this.value) {
+            
+           
+        } else if(value > this.value){
+            
+        }
+    }
+
+    deleteValue(value){
+        
+        
+    }
+
+    setCoordinates(x, y) {
+        if(this.isFilled()) {
+          
+        }
+    }
+
+    
+    search(value) {
+       
+    }
+
+    searchByPos(x, y) {
+       
+    }
+
+    // Draws this node's upper level edge, if the node has a parent
+    drawEdge() {
+       
+    }
+
+    // Draws this node's circular face
+    drawNode() {
+       
+    }
+
+   
+    draw() {
+       
+    }
+
+    // Redraws a singular node on the tree with no side-effects
+    redraw() {
+        
+    }
+
+    // Recursively sets the color and edge color of this node and all nodes
+    // below it to the specified color
+    recursivePaint(color) {
+      
+    }
+
+    // Sets the color and edge color of this node, and redraws the node
+    paint(color) {
+       
+    }
+
+    // Recursively set the appearnace of this node and all nodes below it to
+    // defaults for the class
+    resetVisuals() {
+       
+    }
+}
